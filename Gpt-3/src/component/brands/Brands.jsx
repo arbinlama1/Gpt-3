4c61dda11a6bd7949413a2b303543aeca8739ab8
arbinlama1 <tamangarbin442@gmail.com>
2025-11-10 20:10:48 +0545

    Initialize the folder and Component

---
+++ Gpt-3/src/component/brands/Brands.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './brands.css'
+const Brands = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Brands
---
+++ Gpt-3/src/component/index.jsx
@@ -0,0 +1,5 @@
+export {default as Article } from './article/Article';
+export {defaul as Brands} from './brands/Brands';
+export {defaul as Cta} from './cta/Cta';
+export {defaul as Feature} from './feature/Feature';
+export {defaul as Navbar} from './navbar/Navbar';
