518752ea25f25be65b108debd9f7bc7cb193542e
arbinlama1 <tamangarbin442@gmail.com>
2025-11-19 14:38:54 +0545

    build: implement the section brands and what_gpt3

--- Gpt-3/src/containers/whatGpt3/WhatGpt3.jsx
+++ Gpt-3/src/containers/whatGpt3/WhatGpt3.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './whatGpt3.css';
+import { Feature } from '../../component';
 const WhatGpt3 = () => {
   return (
-    <div>
-      <h1>WhatGPT-3</h1>
+    <div className="gpt3__whatgpt3 section__margin" id="whatgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whtagpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
