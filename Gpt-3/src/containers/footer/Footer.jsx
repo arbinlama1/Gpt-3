1615a7442e80d69310c3b133890f3fb9ff589ecd
arbinlama1 <tamangarbin442@gmail.com>
2025-11-21 15:54:49 +0545

    Fixed: fixing the class name bugs section__padding

--- Gpt-3/src/containers/footer/Footer.jsx
+++ Gpt-3/src/containers/footer/Footer.jsx
@@ -3,7 +3,7 @@ import logo from '/images/logo.svg'
 import './footer.css';
 const Footer = () => {
   return (
-   <div className="gpt3__footer">
+   <div className="gpt3__footer section__padding">
     <div className="gpt3__footer-heading">
       <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
