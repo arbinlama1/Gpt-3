9495a988560e8d228eda1058e032f5a3fd02f3e8
arbinlama1 <tamangarbin442@gmail.com>
2025-11-21 15:38:24 +0545

    build: building the footer section and wrap up the project

--- Gpt-3/src/containers/footer/Footer.jsx
+++ Gpt-3/src/containers/footer/Footer.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import logo from '/images/logo.svg'
 import './footer.css';
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+   <div className="gpt3__footer">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={logo} alt="logo" />
+        <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Medai</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Crechterwoord K12 182 Dk Alknjkcb</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
+    </div>
+    <div className="gpt3__footer-copyright">
+      <p>© 2021 GPT-3. All rights reserved.</p>
+    </div>
+   </div>
   )
 }
 
